afabee5a8919e7d4f5e9cc1bc1d004f43587b62f
Aravind <ajey33@gmail.com>
2020-04-25 04:19:35 +0530

    Goolge assistant webhoot

--- server.js
+++ server.js
@@ -206,9 +206,10 @@ app.post('/api/logout', auth, async (req,res) => {
     }
 })
 
-app.post('/webhook', async (req, res) => {
+app.post('/webhook', (req, res) => {
     try{
-        return res.json({
+        console.log(req.body)
+        res.status(200).json({
             speech: 'Speech text from API',
             displayText: 'Display Text from API',
             source: 'API'
