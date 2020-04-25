8cfbd41052040cb40d235367dd8e2dc5370553c4
Aravind <ajey33@gmail.com>
2020-04-25 21:38:58 +0530

    disabling sms api

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,11 +5,12 @@ var user = require('mongoose').model('user')
 const auth = async (req, res, next) => {
     var token = req.header('Authorization')
     if (token == undefined) {
-        res.status(200).json({ message: "Not Authorized to access this resource without a valid token" })
+        res.status(403).json({ message: "Not Authorized to access this resource" })
     } else {
         token = token.replace('Bearer ', '')
         const data = jwt.verify(token, process.env.JWT_KEY)
         try {
+            console.log(data)
             const usr = await user.findOne({ email: data.email })
             if (!usr) {
                 res.status(200).json({ message: "User not registered" });
@@ -24,4 +25,4 @@ const auth = async (req, res, next) => {
     }
 }
 
-    module.exports = auth;
+module.exports = auth;
